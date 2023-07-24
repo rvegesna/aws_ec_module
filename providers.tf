a56a8ea331ac1cd563b314622be09a7342110f8e
rvegesna <rvarma.vegesna@gmail.com>
2023-07-24 12:29:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,10 +9,10 @@ terraform {
 }
 provider "aws" {
   region     = "us-east-1" 
-  #access_key = "AKIAX5YGRPOAKWHNJTOB"
-  #secret_key = "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"
-  assume_role {
-    role_arn = "arn:aws:iam::544937704320:role/ra-assume"
-    session_name = "ec2"
- }
+  access_key = "AKIAX5YGRPOAKWHNJTOB"
+  secret_key = "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"
+  #assume_role {
+   # role_arn = "arn:aws:iam::544937704320:role/ra-assume"
+   # session_name = "ec2"
+ #}
 }
