acde518ff217601fb9e8e2a9158fcf01020e1759
rvegesna <rvarma.vegesna@gmail.com>
2023-07-24 12:38:23 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.62.0"
-    }
-  }
-}
+#terraform {
+#  required_version = ">= 0.13"
+#  required_providers {
+#    aws = {
+#      source = "hashicorp/aws"
+#      version = "4.62.0"
+#    }
+#  }
+#}
 provider "aws" {
   region     = "us-east-1" 
   access_key = "AKIAX5YGRPOAKWHNJTOB"
